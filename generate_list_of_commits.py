1213f0739a4f88f5e9b87aa7972e88e714ab78bf
Tyler Lyle <lylet@allegheny.edu>
2017-11-17 02:18:13 -0500

    refactored and added a new function

--- generate_list_of_commits.py
+++ generate_list_of_commits.py
@@ -3,12 +3,14 @@ from dulwich import porcelain
 from contextlib import redirect_stdout
 from io import StringIO
 
-def display_list_of_commits(file_path):
+def import_commits_as_string(file_path):
 
+
+	#Constants
 	NEW_OUTSTREAM = StringIO()
+	
 	commit_str = ""
 
-	
 	#Opens repo at this file
 	path = file_path
 
@@ -20,12 +22,14 @@ def display_list_of_commits(file_path):
 
 	#Porcelain automatically writes to the terminal, to override this we must redirect the stdout with our new outstream saved in NEW_OUTSTREAM.  This allows us to save the log as a string so we can manipulate it.
 	
-	with redirect_stdout(new_outsteam):
+	with redirect_stdout(NEW_OUTSTREAM):
 		porcelain.log(repo)
 	
 	commit_str = f.getvalue()
 	
 	return commit_str
+
+def generate_list_of_commits(str_of_commits):
 
 	
 
