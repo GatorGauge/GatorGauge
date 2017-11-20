a87f340b99cc7bff72db8d2a3d0802d8d7388c6d
Zachary Andrews <andrewsz@allegheny.edu>
2017-11-20 15:47:29 -0500

    added error messages

--- gatorGauge.py
+++ gatorGauge.py
@@ -1,15 +1,27 @@
+""" main file in the GatorGauge system """
 import sys
-
+# local imports
 import github_clone_all
 import parse_args
 import defaults
 import read_markdown
+
 if __name__ == "__main__":
     args = parse_args.parse_args(sys.argv[1:])
-    print(args)
-    if args.get is True and defaults.PROJECT == "" and args.project == "":
-        print("\tERROR: '--get' requires either a default project name or a supplied '--project' flag")
+    # checks if the required information is entered
+    if args.token == "" and defaults.TOKEN == "" and args.get is True and defaults.PROJECT == "" and args.project == "":
+        print("\tERROR: A Github token is required for the system to run. Please enter one in either config.ini or in the command line with the --token flag")
+        print("\tERROR: '--get' requires either a default project name (in config.ini) or a supplied '--project' flag")
         quit()
+        
+    elif args.token == "" and defaults.TOKEN == "":
+        print("\tERROR: A Github token is required for the system to run. Please enter one in either config.ini or in the command line with the --token flag")
+        quit()
+        
+    elif args.get is True and defaults.PROJECT == "" and args.project == "":
+        print("\tERROR: '--get' requires either a default project name in config.ini or supplied with the '--project' flag")
+        quit()
+        
     if args.get is True:
         github_clone_all.get_repositories(args.project, args.prefix, args.token, args.out)
     markdown = read_markdown.read_markdown("test.md")
