3ba37eea73533b35eeddad37aa7db9f87dd17e9c
Nicholas Wojnar <wojnarn@allegheny.edu>
2017-11-20 17:26:12 -0500

    Update generate_list_of_commits.py
    deleted print_commit

--- generate_list_of_commits.py
+++ generate_list_of_commits.py
@@ -34,29 +34,6 @@ def generate_list_of_commits(str_of_commits):
 
 	
 
-def print_commit(commit, decode, outstream=sys.stdout):
-	"""Write a human-readable commit log entry.
-    :param commit: A `Commit` object
-    :param outstream: A stream file to write to
-    """
-    """outstream.write("-" * 50 + "\n")
-    outstream.write("commit: " + commit.id.decode('ascii') + "\n")
-    if len(commit.parents) > 1:
-        outstream.write(
-            "merge: " +
-            "...".join([c.decode('ascii') for c in commit.parents[1:]]) + "\n")
-    outstream.write("Author: " + decode(commit.author) + "\n")
-    if commit.author != commit.committer:
-        outstream.write("Committer: " + decode(commit.committer) + "\n")
-
-    time_tuple = time.gmtime(commit.author_time + commit.author_timezone)
-    time_str = time.strftime("%a %b %d %Y %H:%M:%S", time_tuple)
-    timezone_str = format_timezone(commit.author_timezone).decode('ascii')
-    outstream.write("Date:   " + time_str + " " + timezone_str + "\n")
-    outstream.write("\n")
-    outstream.write(decode(commit.message) + "\n")
-outstream.write("\n") """
-
 
 	
 
