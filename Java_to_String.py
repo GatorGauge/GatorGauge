2f481468f002af9220f0aa186bfa577b8712ceda
Daniel Bonnett <bonnettd@allegheny.edu>
2017-11-20 23:29:14 -0500

    fixed regular expression

--- Java_to_String.py
+++ Java_to_String.py
@@ -1,5 +1,6 @@
 # Importing regex library
 import re
+import pyparsing
 
 
 def read_and_convert(JavaFile):
@@ -11,18 +12,17 @@ def read_and_convert(JavaFile):
     return source_code
 
 
-
 def remove_comments(string):
     """The remove_comments function removes all comments
     from the Java source code returned by read_and_convert.
     This function outputs the entire source code as a string
     with zero whitespaces"""
-    string = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,string) # remove multiline comments of the form (/* comment */)
+    string = re.sub(re.compile("(?<=/\*).*?(?=\*/)",re.DOTALL ) ,"" ,string) # remove multiline comments of the form (/* comment */)
     string = re.sub(re.compile("//.*?\n" ) ,"" ,string) # remove all single line comments
     string = string.replace("\n", "") # replace a new line with a blank character
-    string = string.replace(" ", "") # replace all whitespaces with a blank character
     print(string)
     return string
+
 
 # Function calls
 data = read_and_convert("TipCalculator.java")
