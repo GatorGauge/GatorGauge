2ec1aae69de35b481dadd7a496bb10c8da9906b3
bristola <bristola@allegheny.edu>
2017-11-20 23:42:09 -0500

    Added code to demo new functions. Quick fix for multiline comments.

--- Java_to_String.py
+++ Java_to_String.py
@@ -1,29 +1,34 @@
 # Importing regex library
 import re
-import pyparsing
+import java_parser
 
 
-def read_and_convert(JavaFile):
+def read_and_convert(javaFile):
     """The read_and_convert function reads in a Java File
     and returns the source code from the file, including comments"""
-    with open(JavaFile, 'r') as src:
+    with open(javaFile, 'r') as src:
         source_code = src.read()
 
     return source_code
 
 
-def remove_comments(string):
+def remove_comments(javaString):
     """The remove_comments function removes all comments
     from the Java source code returned by read_and_convert.
     This function outputs the entire source code as a string
     with zero whitespaces"""
-    string = re.sub(re.compile("(?<=/\*).*?(?=\*/)",re.DOTALL ) ,"" ,string) # remove multiline comments of the form (/* comment */)
-    string = re.sub(re.compile("//.*?\n" ) ,"" ,string) # remove all single line comments
-    string = string.replace("\n", "") # replace a new line with a blank character
-    print(string)
-    return string
+    javaString = re.sub(re.compile("(?<=/\*).*?(?=\*/)",re.DOTALL ) ,"" ,javaString) # remove multiline comments of the form (/* comment */)
+    javaString = re.sub(re.compile("//.*?\n" ) ,"" ,javaString) # remove all single line comments
+    javaString = javaString.replace("\n", "") # replace a new line with a blank character
+    javaString = javaString.replace("/**/","") # remove comment residue
+    return javaString
 
 
-# Function calls
+# Test code for demonstration
 data = read_and_convert("TipCalculator.java")
-remove_comments(data)
+newData = remove_comments(data)
+
+print("Number of lines = "+str(java_parser.getNumberOfLines(newData)))
+print("Number of variables = "+str(java_parser.getNumberOfVariables(newData)))
+print("Number of methods = "+str(java_parser.getNumberOfMethods(newData)))
+print("Number of classes = "+str(java_parser.getNumberOfClasses(newData)))
