ad4b82eb37a9b498978159e86e8e68beb04b3491
jerfenson <cerdamejiaj@allegheny.edu>
2017-11-27 19:11:43 -0500

    I'm putthing the function that team 4 created to read all the files from the repos. We have to change a few thing and take out in order to make it work for our program.

--- Java_to_String.py
+++ Java_to_String.py
@@ -3,6 +3,33 @@ import re
 import java_parser
 
 
+
+
+
+def read_file(filePath):
+    """ reads through and creates a list of lists from inputted file """
+    """ can be adapted to fit other group needs """
+    content = list()
+    with open(filePath,'r') as mark:
+        # to add additional file types just use the .endswith() function to specifiy the file type
+        # then add in how the file should be broken up into a list with the .split() function or
+        # whatever functionality is needed
+        if filePath.endswith(".md"):
+            lines = mark.read().replace("\n"," ").split('. ') # removes newline characters and breaks up file into sentences for NLP analysis
+        elif filePath.endswith(".java"):
+            lines = mark.read().split('\n')
+        else:
+            lines = mark.read().split('\n')
+    for line in lines:
+        # if filePath.endswith('README.md'): # formats README.md output, uncomment if needed
+            # line = re.sub(r'(?s)(#)(.*?)(  )', '', line).strip()  # annoying line, removes section headers from README.md files
+        nextLine = list()
+        if not line == '' and not '#' in line: # removes unnecessary lines and headers
+            nextLine.append(line)
+            content.append(nextLine)
+    return content # list of lists
+
+
 def read_and_convert(javaFile):
     """The read_and_convert function reads in a Java File
     and returns the source code from the file, including comments"""
