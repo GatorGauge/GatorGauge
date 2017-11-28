75632025524a806f3c45671a7c396d630bc017dd
Jenna Millin <jennamillin@Jennas-MacBook-Pro.local>
2017-11-28 14:50:22 -0500

    added comments

--- parse_args.py
+++ parse_args.py
@@ -1,17 +1,17 @@
 """ parse arguments inputted via command line """
 import argparse
 
-import defaults
+import defaults #get defaults form defaults.py
 
 def parse_args(args):
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    
-    parser.add_argument('--get',
+
+    parser.add_argument('--get', #downloads repositories: defaults are used if no prefix or project is specified
                             default=False,
                             help="Download the repositories.\nIf not default project or prefix, must use '--project' or '--prefix'",
                             action="store_true")
-                            
+
     parser.add_argument('--list',
                             default=".",
                             const="all",
@@ -21,23 +21,23 @@ def parse_args(args):
     parser.add_argument('--read',
                             default="",
                             help="Read the information in a given file")
-                            
+
     parser.add_argument('--project',
                             default=defaults.PROJECT,
                             help='GitHub project to scan, default: ' + defaults.PROJECT)
-                            
+
     parser.add_argument('--prefix',
                             default=defaults.PREFIX,
                             help='Prefix on projects to match (default: match all projects)')
-                            
+
     parser.add_argument('--token',
                             default=defaults.TOKEN,
                             help='GitHub API token')
-                            
+
     parser.add_argument('--out',
                             default=defaults.OUT,
                             help='Destination directory for GitHub clones (default: current directory)')
 
     args = parser.parse_args()
-    
+
     return args
