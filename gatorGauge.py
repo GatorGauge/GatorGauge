4960a1757705a3cc7c0f17d3a42490102c6ff46b
sutterj <sutterj@allegheny.edu>
2017-11-28 14:39:52 -0500

    updated

--- gatorGauge.py
+++ gatorGauge.py
@@ -20,22 +20,26 @@ if __name__ == "__main__":
         prefix = defaults.PREFIX
         out = defaults.OUT
         if command == "get":
-            token = str(input("Enter the token for the repo: "))
-            project = str(input("Enter the name of the project: "))
-            ask_prefix = str(input("Would you like to specify a prefix? (Y/N): "))
-            while ask_prefix not in rSet:
-                ask_prefix = str(input("Please enter Y or N: "))
-            if ask_prefix == "Y" or ask_prefix == "y":
-                prefix = str(input("Enter the prefix for the repo: "))
-            elif ask_prefix == "N" or ask_prefix == "n":
-                prefix = defaults.PREFIX
-            ask_out = str(input("Would you like to specify the destiantion? (Y/N): "))
-            while ask_out not in rSet:
-                ask_out = str(input("Please enter Y or N: "))
-            if ask_out == "Y" or ask_out == "y":
-                ask_out = str(input("Enter the destination: "))
-            elif ask_out == "N" or ask_out == "n":
-                out = defaults.OUT
+            if defaults.TOKEN == "":
+                token = str(input("GatorGauge requires a GitHub token.  Enter the token for the repo: "))
+            if defaults.PROJECT == "":
+                project = str(input("Enter the name of the project: "))
+            if defaults.PREFIX == "":
+                ask_prefix = str(input("Would you like to specify a prefix? (Y/N): "))
+                while ask_prefix not in rSet:
+                    ask_prefix = str(input("Please enter Y or N: "))
+                if ask_prefix == "Y" or ask_prefix == "y":
+                    prefix = str(input("Enter the prefix for the repo: "))
+                elif ask_prefix == "N" or ask_prefix == "n":
+                    prefix = defaults.PREFIX
+            if defaults.OUT == ".":
+                ask_out = str(input("Would you like to specify the destiantion? (Y/N): "))
+                while ask_out not in rSet:
+                    ask_out = str(input("Please enter Y or N: "))
+                if ask_out == "Y" or ask_out == "y":
+                    ask_out = str(input("Enter the destination: "))
+                elif ask_out == "N" or ask_out == "n":
+                    out = defaults.OUT
             github_clone_all.get_repositories(project, prefix, token, out)
         elif command == "help":
             print("help")
