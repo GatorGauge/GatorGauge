73e78121a61961097f839088b64b4a48744d61a2
Jesse Del Greco <delgrecoj@allegheny.edu>
2017-11-28 14:06:01 -0500

    refactor ratios testcases

--- tests/test_parse_comments.py
+++ tests/test_parse_comments.py
@@ -1,44 +1,104 @@
-"""Test parse_comments functions."""
-import parse_comments
+
+""" Testcases for comment parsing """
+import parse_comments as pc
+
+JAVA_SOURCE = """
+//comment_1
+// comment_2
+ // comment_3
+  // comment_4
+   // comment_5
+    // comment_6
+     // comment_7
+      // comment_8
+       // comment_9
+	// comment_10_tab
+
+}//comment_11
+} //comment_12
+} // comment_13
+return "// comment_14";
+
+/*comment_15*/
+/* comment_16*/
+/* comment_17 */
+
+/* comment_18
+*/
+
+/*
+comment_19
+*/
+
+/*
+ * comment_20
+ */
+"""
 
 
-def test_get_ratio_of_singleline_comments_to_source_code_with_one_comment():
-    java_string = """/**
-    * A class to print out "Hello, world!"
-    * Another line
-    */
-    class HelloWorld {
-        /**
-        * Prints out "Hello, world!"
-        */
-        public static void main(String args[]) {
-            System.out.println("Hello, world!");    // print statement
-        }
-
-    }
-    """
-
-    expected_ratio = 0.2
-    actual_ratio = parse_comments.get_ratio_of_singleline_comments_to_source_code(java_string)
-    assert actual_ratio == expected_ratio
+def test_comment_bol():
+    """ comment at beginning-of-line """
+    comments = pc.list_singleline_java_comments(JAVA_SOURCE)
+    assert "comment_1" in comments
+    assert "comment_2" in comments
 
 
-def test_get_ratio_of_multiline_comments_to_source_code_with_two_comments():
-    java_string = """/**
-    * A class to print out "Hello, world!"
-    * Another line
-    */
-    class HelloWorld {
-        /**
-        * Prints out "Hello, world!"
-        */
-        public static void main(String args[]) {
-            System.out.println("Hello, world!");    // print statement
-        }
+def test_comment_space_indented_bol():
+    """ BOL comment indented with spaces """
+    comments = pc.list_singleline_java_comments(JAVA_SOURCE)
+    assert "comment_3" in comments
+    assert "comment_4" in comments
+    assert "comment_5" in comments
+    assert "comment_6" in comments
+    assert "comment_7" in comments
+    assert "comment_8" in comments
+    assert "comment_9" in comments
 
-    }
-    """
 
-    expected_ratio = 0.4
-    actual_ratio = parse_comments.get_ratio_of_multiline_comments_to_source_code(java_string)
-    assert actual_ratio == expected_ratio
+def test_comment_tab_indented_bol():
+    """ BOL comment indented with tabs """
+    comments = pc.list_singleline_java_comments(JAVA_SOURCE)
+    assert "comment_10_tab" in comments
+
+
+def test_comment_eol():
+    """ comment at end-of-line """
+    comments = pc.list_singleline_java_comments(JAVA_SOURCE)
+    assert "comment_11" in comments
+    assert "comment_12" in comments
+    assert "comment_13" in comments
+
+
+def test_quoted_comment():
+    """ comment within quoted string """
+    comments = pc.list_singleline_java_comments(JAVA_SOURCE)
+    assert "comment_14" not in comments
+
+
+def test_multiline_singleline():
+    """ multiline comment on singleline """
+    comments = pc.list_multiline_java_comments(JAVA_SOURCE)
+    assert "comment_15" in comments
+    assert "comment_16" in comments
+    assert "comment_17" in comments
+
+
+def test_multiline_twoline():
+    """ multiline comment across two lines """
+    comments = pc.list_multiline_java_comments(JAVA_SOURCE)
+    assert "comment_18" in comments
+
+
+def test_multiline_tripleline():
+    """ multiline comment across three lines """
+    comments = pc.list_multiline_java_comments(JAVA_SOURCE)
+    assert "comment_19" in comments
+    assert "comment_20" in comments
+
+
+def test_count_comments():
+    """ check all comments are caught """
+    singleline_comments = pc.list_singleline_java_comments(JAVA_SOURCE)
+    multiline_comments = pc.list_multiline_java_comments(JAVA_SOURCE)
+    assert len(singleline_comments) == 13
+    assert len(multiline_comments) == 6
---
+++ tests/test_parse_comments_ratios.py
@@ -0,0 +1,29 @@
+"""Test parse_comments functions."""
+import parse_comments as pc
+
+
+JAVA_STRING = """/**
+* A class to print out "Hello, world!"
+* Another line
+*/
+class HelloWorld {
+    /**
+    * Prints out "Hello, world!"
+    */
+    public static void main(String args[]) {
+        System.out.println("Hello, world!");    // print statement
+    }
+
+}
+"""
+
+def test_get_ratio_of_singleline_comments_to_source_code_with_one_comment():
+    actual_ratio = \
+        pc.get_ratio_of_singleline_comments_to_source_code(JAVA_STRING)
+    assert actual_ratio == 0.2
+
+
+def test_get_ratio_of_multiline_comments_to_source_code_with_two_comments():
+    actual_ratio = \
+        pc.get_ratio_of_multiline_comments_to_source_code(JAVA_STRING)
+    assert actual_ratio == 0.4
