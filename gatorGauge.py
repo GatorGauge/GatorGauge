7a8b80bcc9f89265038e0f64a77f2ccfe2ec2f47
sutterj <sutterj@allegheny.edu>
2017-11-28 14:43:27 -0500

    updated

--- gatorGauge.py
+++ gatorGauge.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
                     prefix = str(input("Enter the prefix for the repo: "))
                 elif ask_prefix == "N" or ask_prefix == "n":
                     prefix = defaults.PREFIX
-            if defaults.OUT == ".":
+            if defaults.OUT == "":
                 ask_out = str(input("Would you like to specify the destiantion? (Y/N): "))
                 while ask_out not in rSet:
                     ask_out = str(input("Please enter Y or N: "))
