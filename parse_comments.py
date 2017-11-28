69b907d9dab9d3a11fe5c5166a4b593fad7b6ba5
Jesse Del Greco <delgrecoj@allegheny.edu>
2017-11-28 13:49:38 -0500

    squash scope bug with whitespace trimming

--- parse_comments.py
+++ parse_comments.py
@@ -17,18 +17,20 @@ def list_singleline_java_comments(java_string):
     """Return list of singleline Java comments in the java_string."""
     pattern = re.compile(SINGLELINECOMMENT_RE_JAVA, re.MULTILINE)
     singleline_comments = pattern.findall(java_string)
+    trimmed_comments = []
     for comment in singleline_comments:
-        comment = comment.strip()
-    return singleline_comments
+        trimmed_comments.append(comment.strip())
+    return trimmed_comments
 
 
 def list_multiline_java_comments(java_string):
     """Count the number of multiline Java comments in the java_string."""
     pattern = re.compile(MULTILINECOMMENT_RE, re.MULTILINE)
     multiline_comments = pattern.findall(java_string)
+    trimmed_comments = []
     for comment in multiline_comments:
-        comment = comment.strip()
-    return multiline_comments
+        trimmed_comments.append(comment.strip())
+    return trimmed_comments
 
 
 def count_singleline_java_comments(java_string):
@@ -63,23 +65,23 @@ def get_ratio_of_multiline_comments_to_source_code(java_string):
     return float(number_of_multiline_comments / total_number_of_lines)
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    with open('./java/HelloWorld.java', 'r') as java_file:
-        java_string = java_file.read()
+#     with open('./java/HelloWorld.java', 'r') as java_file:
+#         java_string = java_file.read()
 
-    comments = list_singleline_java_comments(java_string)
-    print("\nSingleline comments\n-------------------")
-    for comment in comments:
-        print(repr(comment))
-    print("Number of singleline comments: " + str(count_singleline_java_comments(java_string)))
+#     comments = list_singleline_java_comments(java_string)
+#     print("\nSingleline comments\n-------------------")
+#     for comment in comments:
+#         print(repr(comment))
+#     print("Number of singleline comments: " + str(count_singleline_java_comments(java_string)))
 
-    print("Ratio of singleline comments to total Java source code lines: " + str(get_ratio_of_singleline_comments_to_source_code(java_string)))
+#     print("Ratio of singleline comments to total Java source code lines: " + str(get_ratio_of_singleline_comments_to_source_code(java_string)))
 
-    comments = list_multiline_java_comments(java_string)
-    print("\nMultiline comments\n------------------")
-    for comment in comments:
-        print(repr(comment))
-    print("Number of multiline comments: " + str(count_multiline_java_comments(java_string)))
+#     comments = list_multiline_java_comments(java_string)
+#     print("\nMultiline comments\n------------------")
+#     for comment in comments:
+#         print(repr(comment))
+#     print("Number of multiline comments: " + str(count_multiline_java_comments(java_string)))
 
-    print("Ratio of multiline comments to total Java source code lines: " + str(get_ratio_of_multiline_comments_to_source_code(java_string)))
+#     print("Ratio of multiline comments to total Java source code lines: " + str(get_ratio_of_multiline_comments_to_source_code(java_string)))
