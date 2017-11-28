ad46b92a71ccf8be33e311995e46f19d1e16b8a4
Andre Bryan <bryana@aldenv166.allegheny.edu>
2017-11-28 15:48:14 -0500

    Stats done

--- analyze_java.py
+++ analyze_java.py
@@ -1,8 +1,22 @@
 import file_list
 import java_to_string
 import java_parser as p
+import numpy
+def getStatistics(listVar, listMeth, listClass, listLine):
+    print("Average number of variables: ", numpy.mean(listVar))
+    print("Average number of methods: ", numpy.mean(listMeth))
+    print("Average number of classes: ",numpy.mean(listClass))
+    print("Average number of lines: ", numpy.mean(listLine))
 
+    print("Standard deviation of variables: ", numpy.std(listVar))
+    print("Standard deviation of methods: ", numpy.std(listMeth))
+    print("Standard deviation of classes: ", numpy.std(listClass))
+    print("Standard deviation of lines: ", numpy.std(listLine))
 def analyze_java(out):
+    variableList = []
+    methodList= []
+    classList = []
+    lineList = []
     print("Analyzing java files:")
     java_files = file_list.list_files("java", out, True)
     for java_file in java_files:
@@ -10,9 +24,14 @@ def analyze_java(out):
         java_string = java_to_string.remove_comments(java_string)[:]
         try:
             print (java_file)
-            print ("Number of variables: ", p.getNumberOfVariables(java_string))
-            print ("Number of methods: ", p.getNumberOfMethods(java_string))
-            print ("Number of classes: ", p.getNumberOfClasses(java_string))
-            print ("Number of lines: ", p.getNumberOfLines(java_string))
+            variableList.append(p.getNumberOfVariables(java_string))
+            methodList.append(p.getNumberOfMethods(java_string))
+            classList.append(p.getNumberOfClasses(java_string))
+            lineList.append(p.getNumberOfLines(java_string))
         except(Exception):
             print ("error")
+    #print(variables)
+    #print(methods)
+    #print(classes)
+    #print(lines)
+    getStatistics(variableList, methodList, classList, lineList)
