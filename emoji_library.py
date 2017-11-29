0f8ea9bb6861d650f51fe88f670567c8ce5d452d
Karol Vargas <vargask@aldenv169.allegheny.edu>
2017-11-29 18:12:40 -0500

    emoji counter which returns array with emoji counts

--- emoji_library.py
+++ emoji_library.py
@@ -1,5 +1,38 @@
-emoji = 
+import emoji
+from emoji_data_python import emoji_data
+from .emoji_char import EmojiChar
 
-def if_is_emoji(list_of_responses):
-    """Intakes a list of responses"""
-    
+def emoji_counter(commit_str):
+    emoticon1 = re.finditer(ru'[:smile:]', s)
+    smileFace = sum(1 for _ in commit_str)
+
+    emoticon2 = re.finditer(ru'[:rage:]', s)
+    rageFace = sum(1 for _ in commit_str)
+
+    emoticon3 = re.finditer(ru'[:confused:]', s)
+    confusedFace = sum(1 for _ in commit_str)
+
+    emoticon4 = re.finditer(ru'[:thumbsdown:]', s)
+    thumbsdown = sum(1 for _ in commit_str)
+
+    emoticon5 = re.finditer(ru'[:hourglass:]', s)
+    hourglass = sum(1 for _ in commit_str)
+
+    emoticon6 = re.finditer(ru'[:sob:]', s)
+    sobFace = sum(1 for _ in commit_str)
+
+    emoticon7 = re.finditer(ru'[:neutral_face:]', s)
+    neutralFace = sum(1 for _ in commit_str)
+
+    emoticon8 = re.finditer(ru'[:sunglasses:]', s)
+    sunglassesFace = sum(1 for _ in commit_str)
+
+    emoticon9 = re.finditer(ru'[:raised_hands:]', s)
+    raisedHands = sum(1 for _ in commit_str)
+
+    emoticon10 = re.finditer(ru'[:smiling_imp:]', s)
+    smilingImp = sum(1 for _ in commit_str)
+
+    emojiData = [smileFace, rageFace, confusedFace, thumbsdown, hourglass, sobFace, neutralFace, sunglassesFace, raisedHands, smilingImp ]
+
+    return emojiData
