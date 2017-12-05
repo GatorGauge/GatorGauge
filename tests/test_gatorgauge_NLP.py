4175b52b43a71b2eac8c94554c65907574e270ed
Zachary Andrews <andrewsz@allegheny.edu>
2017-12-05 16:07:31 -0500

    Fixed docstrings

--- tests/test_gatorgauge_NLP.py
+++ tests/test_gatorgauge_NLP.py
@@ -2,7 +2,7 @@ import gensim
 import pytest
 import refl_gensim
 
-'''tests the NLP/gensim'''
+"""tests the NLP/gensim"""
 
 def test_create_tokens_none_repeat():
     list_responses = [['I am testing'], ['this is a test'], ['make my tokens']]
