1d0baff4cc4331368021e5e0dbf66ae2ee208457
[Chris Cook] <[cookc@allegheny.edu]>
2017-12-05 14:38:26 -0500

    Added first test case to testing commits

--- tests/test_parse_commits.py
+++ tests/test_parse_commits.py
@@ -1,6 +1,9 @@
-'''Test Suite for parse_commits.py''
+'''Test Suite for parse_commits.py'''
 
 import pytest
 import parse_commits
 
-'''Tests the commits that are being parsed'''
+
+def test_num_commits():
+    '''Test the number of commits that are being parsed'''
+
