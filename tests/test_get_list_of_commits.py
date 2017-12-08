355c295360df44c11d98b20758db3252b19c70de
Tyler Lyle <lylet@allegheny.edu>
2017-12-08 15:59:27 -0500

    finished test case implementation

--- tests/test_get_list_of_commits.py
+++ tests/test_get_list_of_commits.py
@@ -1,5 +1,24 @@
 from get_list_of_commits import get_list_of_commits
+from dulwich.repo import Repo
+from pathlib import Path
+from os import mkdir
+import shutil
 
+"""Removes the SampleRepo if one exists"""
+"""This is so setup() wont add double to commits to the repo"""
+shutil.rmtree("SampleRepo")
+
+def setup():
+    file_path = Path("SampleRepo")
+
+    if not (file_path.is_dir()):
+        mkdir("SampleRepo")
+        repo = Repo.init("SampleRepo")
+
+    repo = Repo("SampleRepo")
+    repo.do_commit(b"Initial commit")
+    repo.do_commit(b"ayy lmao")
+    repo.do_commit(b"added lab3")
 
 def test_get_list_of_commits():
     """Test the functionality of getting_list_of_commits"""
@@ -7,4 +26,4 @@ def test_get_list_of_commits():
     list = (get_list_of_commits("SampleRepo"))
     assert len(list) == 3
     assert "Initial commit" in list
-	assert "added lab3" in list
+    assert "added lab3" in list
