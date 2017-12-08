c7e30f1f42d555a118180439289d4b4eeb9c96df
yeeunmariakim <yeeunmariakim@gmail.com>
2017-12-08 15:36:34 -0500

    adding test cases for get_avg_sentiment

--- tests/test_analyze_sentiment.py
+++ tests/test_analyze_sentiment.py
@@ -33,3 +33,23 @@ def test_get_sentence_sentiment_with_negative_sentence():
     compound_value = sentiment_dict[COMPOUND_KEY]
 
     assert compound_value < 0
+
+
+def test_get_avg_sentiment_with_positive_sentences():
+    """Check that list of obviously positive sentences returns a positive sentiment."""
+    comments = ["I love you!", "I am happy!", "You are wonderful."]
+    avg_sentiment_dict = analyze_sentiment.get_avg_sentiment(comments)
+
+    compound_value = avg_sentiment_dict[COMPOUND_KEY]
+
+    assert compound_value > 0
+
+
+def test_get_avg_sentiment_with_negative_sentences():
+    """Check that list of obviously negative sentences returns a positive sentiment."""
+    comments = ["I hate you!", "I am sad.", "You are awful."]
+    avg_sentiment_dict = analyze_sentiment.get_avg_sentiment(comments)
+
+    compound_value = avg_sentiment_dict[COMPOUND_KEY]
+
+    assert compound_value < 0
