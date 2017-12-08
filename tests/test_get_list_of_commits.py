50898f04b1d0369d4df57e3b4fb4eca242227e17
Tyler Lyle <lylet@allegheny.edu>
2017-12-08 15:14:03 -0500

    added test case

--- tests/test_get_list_of_commits.py
+++ tests/test_get_list_of_commits.py
@@ -1,3 +1,10 @@
-import get_list_of_commits
+from get_list_of_commits import get_list_of_commits
+
 
 def test_get_list_of_commits():
+    """Test the functionality of getting_list_of_commits"""
+    list = []
+    list = (get_list_of_commits("SampleRepo"))
+    assert len(list) == 3
+    assert "Initial commit" in list
+	assert "added lab3" in list
