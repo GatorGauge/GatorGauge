d4d0733afd7cba21f03661dcebd5c452bd7dcb6e
Jerfenson Cerda Mejia <cerdamejiaj@aldenv104.allegheny.edu>
2017-12-10 16:05:29 -0500

    fixing a small error in the test case

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,7 +1,7 @@
 import statistics
 import numpy as np
 
-def test_getStatistics):
+def test_getStatistics():
     """tests the getStatistics functions with a numList list """
     numList = [12, 25, 54, 23, 121, 235, 643, 234, 234, 432]
     statString = statistics.getStatistics(numList)
