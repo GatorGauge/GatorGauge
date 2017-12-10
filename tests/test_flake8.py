fde60aed017fbc8d4b3b05ff00a08f229670e4a7
Jesse Del Greco <delgrecoj@allegheny.edu>
2017-12-10 18:33:33 -0500

    ironically fix flake8 errors within flake8 testcase

--- tests/test_flake8.py
+++ tests/test_flake8.py
@@ -1,19 +1,23 @@
+""" test that python files align to flake8 standards """
+
+
 import glob
 import os
 from flake8.api import legacy as flake8
 
 
-# Testing the python file for flake8 standards
 def test_flake8():
+    """ test python files align to flake8 standards """
 
     # list of all file names to be checked for PEP8
     name_of_files = []
 
     # fill list with all python files found in all subdirectories
-    for root, dirs, files in os.walk("GatorGauge", topdown=False):
+    for root, _, files in os.walk("GatorGauge", topdown=False):
         files = glob.glob(root + "/../*.py")
         name_of_files.extend(files)
 
-    style_guide = flake8.get_style_guide(ignore=["E265", "E501", "E101", "W191"])
+    style_guide = flake8.get_style_guide(ignore=[
+        "E265", "E501", "E101", "W191"])
     report = style_guide.check_files(name_of_files)
     assert report.get_statistics('E') == [], 'Flake8 found violations'
