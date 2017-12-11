c1bb0634608d4328d636b9e6b25e967ce45ed82e
Jesse Del Greco <delgrecoj@allegheny.edu>
2017-12-10 19:34:45 -0500

    partially fix linting errors with gator_gauge.py

--- gator_gauge.py
+++ gator_gauge.py
@@ -8,35 +8,41 @@ import defaults
 import read_file
 
 if __name__ == "__main__":
-    args = parse_args.parse_args(sys.argv[1:])
+    ARGS = parse_args.parse_args(sys.argv[1:])
     # checks if the required information is entered
-    if args.token == "" and defaults.TOKEN == "" and args.get is True and defaults.PROJECT == "" and args.project == "":
-        print("\tERROR: A Github token is required for the system to run. Please enter one in either config.ini or in the command line with the --token flag")
-        print("\tERROR: '--get' requires either a default project name (in config.ini) or a supplied '--project' flag")
+    if ARGS.token == "" and defaults.TOKEN == "" and ARGS.get is True and defaults.PROJECT == "" and ARGS.project == "":
+        print("\tERROR: A Github token is required for the system to run." +
+              " Please enter one in either config.ini or in the command line" +
+              " with the --token flag")
+        print("\tERROR: '--get' requires either a default project name " +
+              "(in config.ini) or a supplied '--project' flag")
         quit()
-    elif args.token == "" and defaults.TOKEN == "":
-        print("\tERROR: A Github token is required for the system to run. Please enter one in either config.ini or in the command line with the --token flag")
+    elif ARGS.token == "" and defaults.TOKEN == "":
+        print("\tERROR: A Github token is required for the system to run." +
+              " Please enter one in either config.ini or in the command line" +
+              " with the --token flag")
         quit()
-    elif args.get is True and defaults.PROJECT == "" and args.project == "":
-        print("\tERROR: '--get' requires either a default project name in config.ini or supplied with the '--project' flag")
+    elif ARGS.get is True and defaults.PROJECT == "" and ARGS.project == "":
+        print("\tERROR: '--get' requires either a default project name" +
+              " in config.ini or supplied with the '--project' flag")
         quit()
 
     # download repositories
-    if args.get is True:
+    if ARGS.get is True:
         github_clone_all.get_repositories(
-            args.project, args.prefix, args.token, args.out)
+            ARGS.project, ARGS.prefix, ARGS.token, ARGS.out)
     # read inputed file
-    if args.read is not "":
-        listFiles = list()
-        for subdir, dirs, files in os.walk(args.out):
+    if ARGS.read is not "":
+        LIST_OF_FILES = list()
+        for subdir, dirs, files in os.walk(ARGS.out):
             for file in files:
-                if file.endswith(args.read):
-                    listFiles.append(os.path.join(subdir, file))
-        for File in listFiles:
+                if file.endswith(ARGS.read):
+                    LIST_OF_FILES.append(os.path.join(subdir, file))
+        for File in LIST_OF_FILES:
             print(*read_file.read_file(File), end="\n\n")
 
     if "--list" in sys.argv[1:]:  # checks if --list was used in command line
-        files = file_list.list_files(
-            args.list, args.out)  # list of files returned
-        for file in files:
+        FILES = file_list.list_files(
+            ARGS.list, ARGS.out)  # list of files returned
+        for file in FILES:
             print(file)
