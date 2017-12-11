1a2778b462d49080078cec29afb7c3fc1af35ae3
Jesse Del Greco <delgrecoj@allegheny.edu>
2017-12-10 19:28:41 -0500

    fix various linting errors with parse_commits

--- parse_commits.py
+++ parse_commits.py
@@ -1,35 +1,40 @@
-from dulwich.repo import Repo
-from dulwich import porcelain
-from contextlib import redirect_stdout
-from io import StringIO
-from import_commits_as_string import import_commits_as_string
+""" parse commit messages """
+
 import pprint
+from import_commits_as_string import import_commits_as_string
+
 
 def number_of_commits(commit_str):
+    """ return count of the number of commits """
+    commit_str = import_commits_as_string("../GatorGaugeSampleRepo")
+    commit_list = commit_str.split(
+        "--------------------------------------------------")
+    commit_count = len(commit_list)-1
+    print(commit_count)
+    return commit_count
 
-	commit_str = import_commits_as_string("../GatorGaugeSampleRepo")
-	commit_list = commit_str.split("--------------------------------------------------")
-	commit_count = len(commit_list)-1
-	print(commit_count)
-	return commit_count
 
 def parse_commits(commit_str):
-	"""Breaks commit string into list"""
-	commit_str = import_commits_as_string("../GatorGaugeSampleRepo")
-	commit_parseCommits = commit_str.split("\n")
-	commit_parseCommits[:] = [item for item in commit_parseCommits if item != '']
-	commit_parseCommits[:] = [item for item in commit_parseCommits if item != '--------------------------------------------------']
-	pprint.pprint(commit_parseCommits)
+    """Breaks commit string into list"""
+    commit_str = import_commits_as_string("../GatorGaugeSampleRepo")
+    commit_parse_commits = commit_str.split("\n")
+    commit_parse_commits[:] = [
+        item for item in commit_parse_commits if item != '']
+    commit_parse_commits[:] = [
+        item for item in commit_parse_commits
+        if item != '--------------------------------------------------']
+    pprint.pprint(commit_parse_commits)
+    return commit_parse_commits
 
-	return commit_parseCommits
 
-def create_commitList(commit_parseCommits, commit_count):
-	number_of_commits = commit_count
-	commit_commitList = commit_parseCommits.append(number_of_commits)
-	print(commit_commitList)
+def create_commit_list(commit_parse_commits, commit_count):
+    """ return list of commits """
+    number_commits = commit_count
+    commit_commit_list = commit_parse_commits.append(number_commits)
+    print(commit_commit_list)
+    return commit_commit_list
 
-	return commit_commitList
 
-create_commitList(parse_commits("../GatorGaugeSampleRepo"),5)
-#parse_commits("../GatorGaugeSampleRepo",5)
-#number_of_commits("../GatorGaugeSampleRepo")
+create_commit_list(parse_commits("../GatorGaugeSampleRepo"), 5)
+# parse_commits("../GatorGaugeSampleRepo",5)
+# number_of_commits("../GatorGaugeSampleRepo")
