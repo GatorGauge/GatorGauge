cc33f0032357d322d7dca738f9bd481fa85adc90
Zach Cole <colez@allegheny.edu>
2017-12-11 15:22:29 -0500

    Update avg_num_comments.py

--- avg_num_comments.py
+++ avg_num_comments.py
@@ -1,27 +1,14 @@
-import java_to_string as j
-import java_parser as p
-import statistics
-import os
+import parse_comments
 
-def avg_num_comments():
-    singleLine = 0
-    multiLine = 0
-    javadoc = 0
+def printAvg():
+    singleLine = parse_comments.count_singleline_java_comments
+    multiLine = parse_comments.count_multiline_java_comments
+    javadoc = parse_comments.count_javadoc_java_comments
+    lineDict = {'single line: ', 'multi-line: ', 'javadoc: '}
 
-    for eachLine in allLines:
-        if eachLine != " " :
-                eachLine = eachLine.replace(" ",""); #remove space
-                eachLine = self.trim(eachLine);      #remove tabIndent
-                if  (iscomment==False):
-                    if(eachLine.strip().startswith("//")): #LINECOMMENT 
-                        singleLine += 1;
-                    if eachLine == "":
-                        blankCount += 1;
-                    if(eachLine.strip().startswith("/*")):
-                        javadoc += 1;
-                        if(not eachLine.strip().endswith("*/")):
-                            iscomment=True
-                else :
-                    commentCount += 1;
-                    if(eachLine.find("*/")):
-                        iscomment=False
+    lineDict['single line: '] = singleline
+    lineDict['multi-line: '] = multiLine
+    lineDict['javadoc: '] = javadoc_comments
+
+    print(lineDict)
+    return lineDict
