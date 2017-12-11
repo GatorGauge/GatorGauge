8ce5ac93bf37cc891a3f770b6ed67ad8cf2592b1
Jesse Del Greco <delgrecoj@allegheny.edu>
2017-12-10 19:20:26 -0500

    fix various linting errors with import_commits_as_string

--- import_commits_as_string.py
+++ import_commits_as_string.py
@@ -1,30 +1,30 @@
-from dulwich.repo import Repo
-from dulwich import porcelain
+""" imports commits as string """
+
 from contextlib import redirect_stdout
 from io import StringIO
+from dulwich.repo import Repo
+from dulwich import porcelain
 
-#function takes in a file_path, points to a given repo
+
 def import_commits_as_string(file_path):
+    """ function takes in a file_path, points to a given repo """
 
     # Constants
-    NEW_OUTSTREAM = StringIO()
+    new_outstream = StringIO()
 
     # Future string of commits
     commit_str = ""
-
-    # Opens repo at this file
-    path = file_path
 
     # sets repo to open from file_path
     repo = Repo(file_path)
 
     # Porcelain automatically writes to the terminal, to override this we must
-    # redirect the stdout with our new outstream saved in NEW_OUTSTREAM.  
-    # This allows us to save the log as a string so we can manipulate it.
+    # redirect the stdout with our new outstream saved in NEW_OUTSTREAM.  This
+    # allows us to save the log as a string so we can manipulate it.
 
-    with redirect_stdout(NEW_OUTSTREAM):
-        porcelain.log(repo, outstream = NEW_OUTSTREAM)
-		
-    commit_str = NEW_OUTSTREAM.getvalue()
-	
+    with redirect_stdout(new_outstream):
+        porcelain.log(repo, outstream=new_outstream)
+
+    commit_str = new_outstream.getvalue()
+
     return commit_str
