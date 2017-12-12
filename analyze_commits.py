8c901c9aaaff5df5fe0700f5c4ab4ff8a6329d64
yeeunmariakim <yeeunmariakim@gmail.com>
2017-12-11 20:43:57 -0500

    adding analyze commits function

--- analyze_commits.py
+++ analyze_commits.py
@@ -2,19 +2,30 @@
 
 from emoji_library import get_emojis_count
 from analyze_sentiment import get_avg_sentiment
+import parse_commits
+import gg_gensim
+from analyze_comments import embed_stats_into_html
 
 
-def analyze_commits(list_of_commits):
+def analyze_commits(out):
     """Execute analysis for commits."""
 
-    print("Number of commits: " + len(list_of_commits))
+    list_of_commits = parse_commits.get_list_of_commits(out)
 
-    # add topical analysis
+    write_string = ""
+    print("Number of commits: " + len(list_of_commits))
+    write_string += "Number of commits: " + len(list_of_commits)
+
+    gg_gensim.gensim_analysis(list_of_commits)
 
     sentiment = get_avg_sentiment(list_of_commits)
     for key, value in sentiment.iteritems():
         print(key, value)
+        write_string += key + ", " + value + "\n"
 
     emojis_count = get_emojis_count(list_of_commits)
     for key, value in emojis_count.iteritems():
         print(key, value)
+        write_string += key + ", " + value + "\n"
+
+    embed_stats_into_html(write_string)
