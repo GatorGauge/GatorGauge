83af95d49ac19f8f53f01347387649acf8a5a98c
Izaak Miller <milleri@aldenv180.allegheny.edu>
2017-12-11 19:18:35 -0500

    Added sum to stat string

--- statistics.py
+++ statistics.py
@@ -11,7 +11,8 @@ def get_statistics(numerical_list):
         str(np.min(numerical_list)) + " max: " + \
         str(np.max(numerical_list)) + " mean: " + \
         str(np.mean(numerical_list)) + " StdDev: " + \
-        str(np.std(numerical_list)) + " iqr: " + str(iqr)
+        str(np.std(numerical_list)) + " iqr: " + \
+        str(iqr) + " total: " + str(np.sum(numerical_list))
     return stat_string
 
 
