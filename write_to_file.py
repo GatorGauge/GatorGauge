8b011b793dde9d9f08c1b8816314f2a3ac671ed0
Izaak Miller <milleri@aldenv180.allegheny.edu>
2017-12-11 19:53:00 -0500

    Changed names to fit standard

--- write_to_file.py
+++ write_to_file.py
@@ -1,10 +1,10 @@
-def WriteToDefaultFile(stringToFile):
+def write_to_default_file(stringToFile):
     f = open('default.txt', 'w')
     f.write(stringToFile)
     f.close()
 
 
-def WriteToExistingFile(stringToFile, FileName):
+def write_to_existing_file(stringToFile, FileName):
     f = open(FileName, 'w')
     f.write(stringToFile)
     f.close()
