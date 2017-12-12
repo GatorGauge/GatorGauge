455bac777f7c465b6328406950071201a62d202c
Izaak Miller <milleri@aldenv180.allegheny.edu>
2017-12-11 19:25:29 -0500

    Added write to default file and write to existing file functions

---
+++ WriteToFile.py
@@ -0,0 +1,9 @@
+def WriteToDefaultFile(stringToFile):
+    f = open('default.txt','w')
+    f.write(stringToFile)
+    f.close()
+
+def WriteToExistingFile(stringToFile, FileName):
+    f = open(FileName, 'w')
+    f.write(stringToFile)
+    f.close()
