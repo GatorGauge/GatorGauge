92b73fecf5cfd91fd6c893155db7375d6466e171
yeeunmariakim <yeeunmariakim@gmail.com>
2017-12-11 19:47:40 -0500

    changing emojis count impl

--- emoji_library.py
+++ emoji_library.py
@@ -1,32 +1,33 @@
 import re
 
 
-def emoji_counter(s):
+def get_emojis_count(list_of_commits):
+    """Count emojis within a list of commits."""
 
-    emoticon1 = re.findall(":smile:",s)
-    emoticon2 = re.findall(":rage:",s)
-    emoticon3= re.findall(":confused:",s)
-    emoticon4 = re.findall(":thumbsdown:",s)
-    emoticon5 = re.findall(":hourglass:",s)
-    emoticon6 = re.findall(":sob:",s)
-    emoticon7 = re.findall(":neutral:",s)
-    emoticon8 = re.findall(":sunglasses:",s)
-    emoticon9 = re.findall(":raisedHands:",s)
-    emoticon10 = re.findall(":smilingImp:",s)
+    smile = rage = confused = thumbsdown = hourglass = sob = \
+        neutral = sunglasses = raisedHands = smilingImp = 0
 
-    emojies = len(emoticon1)+len(emoticon2)+len(emoticon3)+len(emoticon4)+len(emoticon5)+len(emoticon6)+len(emoticon7)+len(emoticon8)+len(emoticon9)+len(emoticon10)
+    for commit in list_of_commits:
+        smile += len(re.findall(":smile:", list_of_commits))
+        rage += len(re.findall(":rage:", list_of_commits))
+        confused += len(re.findall(":confused:", list_of_commits))
+        thumbsdown += len(re.findall(":thumbsdown:", list_of_commits))
+        hourglass += len(re.findall(":hourglass:", list_of_commits))
+        sob += len(re.findall(":sob:", list_of_commits))
+        neutral += len(re.findall(":neutral:", list_of_commits))
+        sunglasses += len(re.findall(":sunglasses:", list_of_commits))
+        raisedHands += len(re.findall(":raisedHands:", list_of_commits))
+        smilingImp += len(re.findall(":smilingImp:", list_of_commits))
 
-    print("Total Emojies: ", emojies)
-    print("SmileyFaces: ", len(emoticon1))
-    print("RageFaces: ", len(emoticon2))
-    print("Confused: ", len(emoticon3))
-    print("thumbsdown: ", len(emoticon4))
-    print("TimeConsuming: ", len(emoticon5))
-    print("Sad: ", len(emoticon6))
-    print("Neutral: ", len(emoticon7))
-    print("Cool: ", len(emoticon8))
-    print("RaisedHands: ", len(emoticon9))
-    print("SmilingImp: ", len(emoticon10))
+    emojis_count = {"smile": smile,
+                    "rage": rage,
+                    "confused": confused,
+                    "thumbsdown": thumbsdown,
+                    "hourglass": hourglass,
+                    "sob": sob,
+                    "neutral": neutral,
+                    "sunglasses": sunglasses,
+                    "raisedHands": raisedHands,
+                    "smilingImp": smilingImp}
 
-if __name__ == "__main__":
-    emoji_counter()
+    return emojis_count
