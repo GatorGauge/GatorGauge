45f7f325f160180f3b0702a7dfff610cd55f412a
[Chris Cook] <[cookc@allegheny.edu]>
2017-12-14 18:28:48 -0500

    Added path that will be tested

--- tests/test_count_commits2.py
+++ tests/test_count_commits2.py
@@ -3,3 +3,11 @@ import sys
 import count_commits.py
 from dulwich.repo import Repo
 from pathlib import Path
+
+def test_path():
+    """Program getting ready for testing"""
+    test_path = Path("SampleRepo")
+    repo_path = "SampleRepo"
+
+    if path.is_dir():
+        
