c812743b2dc6e2dd2e2339f6e47ab3d7e87b567b
[Chris Cook] <[cookc@allegheny.edu]>
2017-12-14 18:38:28 -0500

    Added sample commits and test function for commits

--- tests/test_count_commits2.py
+++ tests/test_count_commits2.py
@@ -10,4 +10,18 @@ def test_path():
     repo_path = "SampleRepo"
 
     if path.is_dir():
-        
+        shutil.rmtree(path="SampleRepo", ignore_errors=Tree)
+
+    mkdir(repo_path)
+    repo = Repo.init(repo_path)
+    repo = Repo(repo_path)
+    repo.do_commit(b"Intial commit for sample repository")
+    repo.do_commit(b"Added README.md file for lab 3")
+    repo.do_commit(b"Deleted function in lab 2")
+    repo.do_commit(b"Push for lab 1")
+    repo.do_commit(b"Random commit XD123")
+
+def test_commits():
+    repo_commits = 5
+    commits_in_repo = counted_num_commits("SampleRepo")
+    assert commits_in_repo == counted_num_commits
