be66b420f15b900e0f61bc7eb624fa3e37005878
Angeline Sporrer <angelinesporrer@Angelines-MacBook-Air.local>
2017-12-17 17:35:57 -0500

    Upload with correct documentation

--- write_to_file.py
+++ write_to_file.py
@@ -1,10 +1,11 @@
-#Not used in this feature, but keeping it so that it could be used in a future feature
+# Not used in this feature, but keeping it so that it could be used in a future feature
 def write_to_default_file(stringToFile):
     f = open('default.txt', 'w')
     f.write(stringToFile)
     f.close()
 
-#Writes java statistics to text file for use in other methods
+
+# Writes java statistics to text file for use in other methods
 def write_to_existing_file(stringToFile, FileName): 
     f = open(FileName, 'w')
     f.write(stringToFile)
