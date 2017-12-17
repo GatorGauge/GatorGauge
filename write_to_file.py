c70d88b8c4973a163bbbd6a69544c8396267802e
Angeline Sporrer <angelinesporrer@Angelines-MacBook-Air.local>
2017-12-17 18:02:39 -0500

    added comments to the functions

--- write_to_file.py
+++ write_to_file.py
@@ -1,9 +1,11 @@
+# Not needed for this version, but could be used as a future enhancement
 def write_to_default_file(stringToFile):
     f = open('default.txt', 'w')
     f.write(stringToFile)
     f.close()
 
 
+# Writes the java statistics to a user-specified text file
 def write_to_existing_file(stringToFile, FileName):
     f = open(FileName, 'w')
     f.write(stringToFile)
