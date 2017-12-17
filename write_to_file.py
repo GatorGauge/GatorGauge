978ee32765aa15e60e14b37bdcdb242aebffddaa
Angeline Sporrer <angelinesporrer@Angelines-MacBook-Air.local>
2017-12-17 17:41:28 -0500

    update with documentation

--- write_to_file.py
+++ write_to_file.py
@@ -6,7 +6,7 @@ def write_to_default_file(stringToFile):
 
 
 # Writes java statistics to text file for use in other methods
-def write_to_existing_file(stringToFile, FileName): 
+def write_to_existing_file(stringToFile, FileName):
     f = open(FileName, 'w')
     f.write(stringToFile)
     f.close()
