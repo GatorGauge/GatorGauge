3b7392e65ed7e03216fbf97bc64a37acd61d4227
Angeline Sporrer <angelinesporrer@Angelines-MacBook-Air.local>
2017-12-17 22:36:19 -0500

    fixed assertion errors

--- tests/test_analyze_java.py
+++ tests/test_analyze_java.py
@@ -4,12 +4,12 @@ import analyze_java as aj
 def test_get_file_paths():
     """Testing to ensure that the get_file_paths function returns the correct list of file paths"""
     path_list = aj.get_file_paths(".java", "test_repos")
-    correct_path = ['test_repos/test-repo-2/src/test.java', 'test_repos/test-repo-3/src/test.java', 'test_repos/test-repo-1/src/test.java']
+    correct_path = ['test_repos/test-repo-1/src/test.java', 'test_repos/test-repo-2/src/test.java', 'test_repos/test-repo-3/src/test.java']
     assert correct_path == path_list
 
 
 def test_get_java_strings():
     """Testing to ensure that the get_java_strings function returns java source code as a string"""
     java_string = aj.get_java_strings("simple_test_repo")
-    correct_string = ['public class test {\n    public static void main(String[] args) {\n        int x = 3;\n        System.out.println(x);\n    }\n}']
+    correct_string = ['public class simple {\n    public static void main(String[] args) {\n        int x = 3;\n        System.out.println(x);\n    }\n}']
     assert java_string == correct_string
